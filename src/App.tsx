258b4a2378626ecf2af1fdb8ff41cf394453963f
84Ebi <ebrahimkhosravani3@gmail.com>
2024-10-12 00:28:21 +0330

    1.0 frontend fully functional

---
+++ src/App.tsx
@@ -0,0 +1,306 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import {
+  a,
+  anbg,
+  anbg1,
+  b,
+  github,
+  mail,
+  linkedin,
+  resume,
+  telegram,
+  windows1,
+} from "./images";
+
+
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState("Home");
+  const navigateToHome = () => setCurrentPage("Home");
+  const navigateToContact = () => setCurrentPage("Contact");
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time (e.g., fetching data, etc.)
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2230); // Adjust the loading time as needed
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <div className='justify-center items-center flex bg-black h-full'>
+          <img src={windows1} alt="loading" className='w-4/5 mt-9 bg-black '/>
+          <img src={a} className='notshown'/>
+          <img src={b} className='notshown'/>
+          <img src={github} className='notshown'/>
+          <img src={linkedin} className='notshown'/>
+          <img src={mail} className='notshown'/>
+          <img src={anbg} className='notshown'/>
+          <img src={anbg1} className='notshown'/>
+          <img src={telegram} className='notshown'/>
+        </div>
+        
+        
+      ) : (
+        <>
+          
+
+          {currentPage === "Home" && (
+            <div className="window-body">
+
+              <div className='fixed align bg-white'>
+                  <menu role="tablist">
+                    <li role="tab" className="cursor-pointer text-xl" aria-selected="true">
+                      <a onClick={navigateToHome}>Home</a>
+                    </li>
+
+                    <li role="tab" className="cursor-pointer text-xl" aria-selected="false">
+                      <a onClick={navigateToContact}>Contact</a>
+                    </li>
+                  </menu>
+                <div className='fixed top-0 right-3 window '>
+                    <img src={a} className='w-10'/>
+                </div>
+              </div>
+              <div className="window flex top-10 mb-9" role="tabpanel">
+                <div className="window w-11/12 h-auto">
+                  <div className="window">
+                    <div className="title-bar">
+                      <div className="title-bar-text text-xl" id="aboutme">
+                        Ebrahim Khosravani <br />
+                        Full Stack Developer
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex justify-center pb-5 pt-5">
+
+                    <div className="window w-4/6">
+                      <div className="items-center text-center">
+                        <div className="window">
+                          <div className="title-bar">
+                            <div className="title-bar-text text-sm"><b >About Me</b></div>
+                          </div>
+                          <div className="window-body text-lg">
+                            <p>
+                              I am Ebrahim Khosravani,<br></br> a <b>Fullstack Developer with a strong academic foundation in Physics from Sharif University of Technology.</b> My primary expertise lies in developing <b>web applications</b>, where I excel at both front-end technologies like<b> React, HTML, and CSS, and back-end frameworks such as Django and Node.js.</b> I am proficient in <b>TypeScript, Python,</b> and a range of tools essential for building scalable and efficient web solutions. With a passion for creating seamless, user-centric digital experiences, I have honed my skills through advanced courses and hands-on projects, particularly in the development of web applications. My background also includes knowledge in AI and a variety of software tools.
+                            </p>
+                          </div>
+
+                        </div>
+
+                      </div>
+                    </div>
+
+                  </div>
+
+                  <div className='pt-8 pb-5 justify-center flex '>
+                          <div className="window w-3/6">
+                            <div className="title-bar">
+                              <div className="title-bar-text text-sm"><b id="skills">skills</b></div>
+                              <div className="title-bar-controls">
+                              </div>
+                            </div>
+                            <div className="window">
+                              <div className=' '>
+                                <div className="   inline  text-sm">
+
+                                  <div className='field-row w-full mt-16 mb-6'>
+                                    <p className='w-24 ml-3'  id='English'>English</p>
+                                    <input id="range26" type="range" min="1" max="7" value="6" />
+                                    <p className='mr-3'>C1</p>
+                                    
+                                  </div>
+
+                                  <div className='field-row w-full mt-6 mb-6'>
+                                    <p className='w-24 ml-3'  id='React'>React</p>
+                                    <input id="range26" type="range" min="1" max="12" value="11" />
+                                    <p className='mr-3'>Advanced</p>
+                        
+                                  </div>
+
+                                  <div className='field-row w-full mt-6 mb-6'>
+                                    <div className='w-24 ml-3' id='Html & CSS'>Html & CSS</div>
+                                    <input id="range26" type="range" min="1" max="9" value="8" />
+                                    <p className='mr-3'>Advanced</p>
+                                        
+                                    </div>
+
+                                  <div className='field-row w-full mt-6 mb-6'>
+                                    <p className='w-24 ml-3' id='Java Script'>Java Script</p>
+                                    <input id="range26" type="range" min="1" max="9" value="8" />
+                                    <p className='mr-3'>Advanced</p>
+                                    
+                                    </div>
+
+                                  <div className='field-row w-full mt-6 mb-6'>
+                                    <p className='w-24 ml-3'  id='Type Script'>Type Script</p>
+                                    <input id="range26" type="range" min="1" max="14" value="13" />
+                                    <p className='mr-3'>Advanced</p>
+                                    
+                                    </div>
+
+                                  <div className='field-row w-full mt-6 mb-6'>
+                                    <div className='w-24 ml-3'  id='Python'>Python</div>
+                                    <input id="range26" type="range" min="1" max="12" value="9" />
+                                    <p className='mr-3'>Intermediate</p>
+                                    
+                                    </div>
+
+                                  <div className='field-row w-full mt-6 mb-6'>
+                                    <p className='w-24 ml-3'  id='Docker'>Docker</p>
+                                    <input id="range26" type="range" min="1" max="14" value="10" />
+                                    <p className='mr-3'>Intermediate</p>
+                                    
+                                    </div>
+
+                                  <div className='field-row w-full mt-6 mb-6'>
+                                    <p className='w-24 ml-3'  id='Django'>Django</p>
+                                    <input id="range26" type="range" min="1" max="9" value="6" />
+                                    <p className='mr-3'>Intermediate</p>
+                                    
+                                    </div>
+
+                                  <div className='field-row w-full mt-6 mb-16'>
+                                    <p className='w-24 ml-3'  id='Node.js'>Node.js</p>
+                                    <input id="range26" type="range" min="1" max="9" value="4" />
+                                    <p className='mr-3'>Basic</p>
+                                    
+                                  </div>
+                                  
+                                </div>
+                              </div>
+
+                            </div>
+                            
+                          </div>
+                          <div className='justify-center inline-block' id="dresume">
+                            <button  className='w-64 h-12 ml-6' >
+                              <a href={resume}  download>Download Resume </a>
+                            </button>
+                            <div className='window w-64 mt-10 ml-6'>
+                              <img src={b}></img>
+                            </div>
+                          </div>
+                    </div>
+
+
+                </div>
+                <div className='right-2 fixed w-32'>
+                  <ul className="tree-view ">
+                    <li><a href="#aboutme">About Me</a></li>
+                    <li><a href="#dresume">My resume</a></li>
+                    <li>
+                      <details>
+                        <summary><a href="#skills">Skills</a></summary>
+                        <ul>
+                          <li><a href='#English'>English</a></li>
+                          <li><a href='#React'>React</a></li>
+                          <li><a href='#Html & CSS'>Html & CSS</a></li>
+                          <li><a href='#Java Script'>Java Script</a></li>
+                          <li><a href='#Type Script'>Type Script</a></li>
+                          <li><a href='#Python'>Python</a></li>
+                          <li><a href='#Docker'>Docker</a></li>
+                          <li><a href='#Django'>Django</a></li>
+                          <li><a href='#Node.js'>Node.js</a></li>
+                        </ul>
+                      </details>
+                    </li>
+                    <li><a href='#info'>Contact Info</a></li>
+                    
+                  </ul>
+                </div>
+
+                
+              </div>
+              <div id='info' className='inline-block bottom-0 mt-3 w-full'>
+                      <div className="title-bar justify-around ">
+                          <div className='title-bar-text text-xl text-center flex'><img src={anbg}  className='w-28 -ml-20 -mr-7 -mb-11 -mt-12'></img>Ebrahim Khosravani</div><div className="title-bar-text text-xl mb-5 mt-5 text-center">Email<br></br> <address ><a href='mailto:ebrahimkhosravani3@gmail.com'>ebrahimkhosravani3@gmail.com</a></address></div><div className='title-bar-text text-xl text-center'>location<br></br> Tehran, Iran </div>
+
+                      </div>
+                    </div>
+            </div>
+            
+
+          )}
+
+
+          {currentPage === "Contact" && (
+            <div className="window-body">
+
+            <div className='fixed align bg-white'>
+                <menu role="tablist">
+                  <li role="tab" className="cursor-pointer text-xl" aria-selected="false">
+                    <a onClick={navigateToHome}>Home</a>
+                  </li>
+
+                  <li role="tab" className="cursor-pointer text-xl" aria-selected="true">
+                    <a onClick={navigateToContact}>Contact</a>
+                  </li>
+                </menu>
+              <div className='fixed top-0 right-3 window '>
+                  <img src={a} className='w-10'/>
+              </div>
+            </div>
+            <div className="window  flex top-10 mb-10" role="tabpanel">
+              <div className="window w-full">
+                <div className="window">
+                  <div className="title-bar">
+                    <div className="title-bar-text text-xl" >
+                      Contact Info
+                    </div>
+                  </div>
+                </div>
+                <div className="flex pb-5 pt-5">
+
+                  <div className="window w-full mr-52 ml-52">
+                    <div className="items-center text-center">
+                      <div className="window items-center">
+                        <div className="title-bar">
+                          <div className="title-bar-text text-sm"><b >Socials</b></div>
+                        </div>
+                        <div className="window-body text-lg">
+                          <div className='inline-block mt-10 mb-10'>
+                            <button className='flex w-full justify-center items-center mt-2 mb-2 h-16' onClick={() => window.open('https://www.linkedin.com/in/ebrahim-khosravani-6a5bbb177', '_blank')}><img className='ml-4 mr-2 mb-3 mt-2 w-8 ' src={linkedin}></img>Linkedin</button>                            
+                            <button className='flex w-full justify-center mt-2 items-center h-16' onClick={() => window.open('https://github.com/84Ebi', '_blank')}><img className='w-10 mb-3 mt-2' src={github}></img>Github</button>
+                            <button className='flex w-full justify-center mt-2 items-center h-16 text-xl ' onClick={() => window.open('mailto:ebrahimkhosravani3@gmail.com', '_blank')}><img className='w-10 mb-3 mt-2 ml-3' src={mail}></img><p className=' mr-3'>ebrahimkhosravani3@gmail.com</p></button>
+                            <button className='flex w-full justify-center mt-2 mb-1 items-center h-16' onClick={() => window.open('https://t.me/ebisssss', '_blank')}><img className='w-10 mb-3 mt-2' src={telegram}></img>Telegram</button>
+
+
+                          </div>
+                        </div>
+
+                      </div>
+
+                    </div>
+                  </div>
+
+                </div>
+
+
+              </div>
+
+
+              
+            </div>
+            <div id='info' className='window inline-block bottom-0 mt-1 h-full w-full'>
+                    <div className="title-bar justify-around ">
+                        <div className='title-bar-text text-xl text-center flex'><img src={anbg}  className='w-28 -ml-20 -mr-7 -mb-11 -mt-12'></img>Ebrahim Khosravani</div><div className="title-bar-text text-xl mb-5 mt-5 text-center">Email<br></br> <address ><a href='mailto:ebrahimkhosravani3@gmail.com'>ebrahimkhosravani3@gmail.com</a></address></div><div className='title-bar-text text-xl text-center'>location<br></br> Tehran, Iran </div>
+                        
+                    </div>
+                  </div>
+                  
+          </div>
+          
+
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+export default App;
---
+++ src/images/index.ts
@@ -0,0 +1,25 @@
+import a from "./a.png";
+import anbg from "./anbg.png";
+import anbg1 from "./anbg1.png";
+import b from "./b.svg";
+import github from "./github.svg";
+import mail from "./mail.svg";
+import linkedin from "./linkedin.svg";
+import resume from "./resume.pdf";
+import telegram from "./telegram.svg";
+import windows from "./windows.gif";
+import windows1 from "./windows1.gif";
+
+export {
+    a,
+    anbg,
+    anbg1,
+    b,
+    github,
+    mail,
+    linkedin,
+    resume,
+    telegram,
+    windows,
+    windows1,
+}
